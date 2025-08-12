b48986dfbdccbcd1eb938c3691fb1c6970654297
Colin Russell <colinrussell@colins-mbp.home>
2025-08-12 11:18:36 -0400

    Add new convenience methods to sort the launches by various means

--- 03-sorting-data-using-sort-descriptors/Starter/RocketLaunches/Views/Launch/LaunchesView.swift
+++ 03-sorting-data-using-sort-descriptors/Starter/RocketLaunches/Views/Launch/LaunchesView.swift
@@ -34,7 +34,7 @@ import SwiftUI
 
 struct LaunchesView: View {
   @State var isShowingCreateModal = false
-  let launchesFetchRequest = RocketLaunch.basicFetchRequest()
+  let launchesFetchRequest = RocketLaunch.fetchRequestSortedByNameAndLaunchDate()
   var launches: FetchedResults<RocketLaunch> {
     launchesFetchRequest.wrappedValue
   }
